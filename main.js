15af7660819d9abb839aca25537a929f95fa2f72
Nícolas Felipe Ferreira Souza <140756005+NicolasFelipe162@users.noreply.github.com>
2024-09-20 12:13:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+alert('ALERT TESTE');
+console.log('CONSOLE TESTE')
+
+var titulo = document.querySelector('h1');
+titulo.textContent = 'TITULO';
+
+var trPaulo = document.querySelector('#paulo');
+console.log(trPaulo);
+
+var tdPeso = trPaulo.querySelector('.info-peso');
+tdPeso.textContent = "150";
